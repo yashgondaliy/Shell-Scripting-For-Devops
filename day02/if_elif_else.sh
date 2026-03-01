8d5693d43b29b4091cdb24ff2f9d70472c1bea68
yashgondaliy <yashgondaliya0@gmail.com>
2026-03-01 05:18:11 +0000

    modified if file

--- day02/if_elif_else.sh
+++ day02/if_elif_else.sh
@@ -3,19 +3,16 @@
 <<info 
 it help to learn if statement
 info
-
 read -p "jetha ne mud k kya dekha: " bandi
-#read -p "jetha ka pyaar %: " pyaar
 
-if [[ $bandi == daya ]];
-then
+if [[ $bandi == daya ]] then
 	echo "jetha is loyal"
-
-#read -p "jetha ka pyaar %: " pyaar
-elif [[ $pyaar -ge 100 ]];
-read -p "jetha ka pyaar %: " pyaar
-then
-	echo "jetha is loyal"
-else
-	echo "jetha is not loyal"
+elif [[ $bandi != daya ]] then
+	read -p "how much you love% $bandi  " pyar
+	if [[ pyar -ge 100 ]] then
+		echo "jetha is loyal"
+	else
+		echo "jetha is not loyal"
+	fi
 fi
+
